5a95176e7bec331b09abff44e064baa4f5058dbd
gelila-d <gelilad1111@gmail.com>
2026-02-11 20:50:32 +0000

    organize the routes

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,9 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js"
 const app = express();
 
-app.get("/api/notes",(req,res)=>{
-    res.status(200).send("you got 5 notes");
-});
+app.use("/api/notes",notesRoutes);
 
-app.post("/api/notes",(req,res)=>{
-    res.status(201).json({message:"post created successfully"});
-});
-
-app.put("/api/notes/:id",(req,res)=>{
-    res.status(200).json({message:"post created successfully"});
-});
-
-app.delete("/api/notes/:id",(req,res)=>{
-    res.status(200).json({message:"post deleted successfully"});
-});
 
 app.listen(5002,()=>{
     console.log("server started on port: 5002");
