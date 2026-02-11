899163da9cca7c9406c387424d15d477cdd61fdf
gelila-d <gelilad1111@gmail.com>
2026-02-11 21:53:57 +0000

    connecting to the database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5002
+connectDB();
 
 app.use("/api/notes",notesRoutes);
 
 
-app.listen(5002,()=>{
-    console.log("server started on port: 5002");
+app.listen(PORT,()=>{
+    console.log("server started on port:", PORT);
 });
