b2461bcbc2bf9944cf6dffa5ca1e14996310e176
gelila-d <gelilad1111@gmail.com>
2026-02-13 18:34:41 +0000

    setting up tailwind CSS

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import { toast } from 'react-hot-toast'
 
 export default function App() {
   return (
-    <div>
-      
+    <div data-theme="cupcake">
+      <button className='btn btn-primary'>click me</button>
+      <button className='btn btn-secondary'>click me</button>
+      <button className='btn btn-accent'>click me</button>
+      <button className='btn btn-ghost'>click me</button>
+      <button className='btn btn-link'>click me</button>
+     
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
   )
 }
